b900f536f3bc061cd2df3419db2f187afb3afdb9
namnguyen <toilanam97@gmail.com>
2019-09-11 16:01:16 +0700

    Link List

---
+++ mainChain.php
@@ -0,0 +1,11 @@
+<?php
+include_once ('LinkList.php');
+$linkedList = new LinkList();
+$linkedList->insertFirst(11);
+$linkedList->insertFirst(22);
+$linkedList->insertLast(33);
+$linkedList->insertLast(44);
+$linkedList->insertLast(43);
+$linkedList->insertLast(21);
+$linkedList->deleteNode(43);
+$linkedList->readNode(5);
